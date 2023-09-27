f0d2e8e73214ebe61f1166153434b893e2d3300a
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2023-09-27 11:03:42 +0200

    commit

--- module_1/speelhal.py
+++ module_1/speelhal.py
@@ -1,11 +1,12 @@
 TICKET = 7.45
 GELD_P_5MIN_PP = 0.37
+aantal_mensen = 5
 
-ticket_totaal = TICKET * 4
+ticket_totaal = TICKET * aantal_mensen
 geld_p_5min_pp_totaal = GELD_P_5MIN_PP * 9
 
 
-geld_p_5min_pp_totaal_1 =  geld_p_5min_pp_totaal * 4
+geld_p_5min_pp_totaal_1 =  geld_p_5min_pp_totaal * aantal_mensen
 
 totaalprijs = ticket_totaal + geld_p_5min_pp_totaal_1
 
--- module_1/speelhal_2.py
+++ module_1/speelhal_2.py
@@ -1,14 +1,17 @@
 TICKET = 7.45
 GELD_P_5MIN_PP = 0.37
+GELD_P_1_MIN_PP = GELD_P_5MIN_PP / 1
 
-ticket_totaal = TICKET * 4
+
+aantal_mensen = 5
+ticket_totaal = TICKET * aantal_mensen
 geld_p_5min_pp_totaal = GELD_P_5MIN_PP * 9
 
 
-geld_p_5min_pp_totaal_1 =  geld_p_5min_pp_totaal * 4
+geld_p_5min_pp_totaal_1 =  geld_p_5min_pp_totaal * aantal_mensen
 
 totaalprijs = ticket_totaal + geld_p_5min_pp_totaal_1
 
 totaalprijs_1 = round(totaalprijs, 5)
 
-print (f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaalprijs_1} euro')
+print (f'Dit geweldige dagje-uit met {aantal_mensen} mensen in de Speelhal met 45 minuten VR kost je maar {totaalprijs_1} euro')
