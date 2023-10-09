c7867e368c4b395d79ab6c1e2fe0a55bb7d3beb8
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2023-10-09 13:14:48 +0200

    elif-statement

--- module_1/deel 3/Meten_Is_Weten.py
+++ module_1/deel 3/Meten_Is_Weten.py
@@ -1,6 +1,14 @@
 cijfer_1 = int(input('print wat is je cijfer 1: '))
 cijfer_2 = int(input('print wat is je cijfer 2: '))
 
-if cijfer_1 >= cijfer_2:
-    print(f'Cijfer 1 is het groter getal {cijfer_1}')
 
+
+if cijfer_1 > cijfer_2:
+   max = cijfer_1
+   print(f'cijfer 1 is een grotere getal dan 2 {max}')
+
+
+elif cijfer_1 < cijfer_2:
+   min = cijfer_1
+   print(f'1 is het kleinste getal: {min}')
+    
