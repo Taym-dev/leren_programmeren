4388f6a170056bad438e8a3d8ba091b1f9765070
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2023-10-09 13:05:06 +0200

    Update Meten_Is_Weten.py

---
+++ module_1/deel 3/Meten_Is_Weten.py
@@ -0,0 +1,6 @@
+cijfer_1 = int(input('print wat is je cijfer 1: '))
+cijfer_2 = int(input('print wat is je cijfer 2: '))
+
+if cijfer_1 >= cijfer_2:
+    print(f'Cijfer 1 is het groter getal {cijfer_1}')
+
