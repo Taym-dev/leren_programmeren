69ec9c7b33ef1de17e6f2e61a47e478ffa5a29b6
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2023-10-10 12:06:33 +0200

    sollicitatie

--- module_1/deel 3/sollicitatie.py
+++ module_1/deel 3/sollicitatie.py
@@ -4,26 +4,24 @@ MAX_GEWICHT = 120
 MIN_LENGTE = 150
 MAX_LENGTE = 220
 
+solliciteren_baan = input('Je kunt solliciteren voor de volgende banen. Kies uit de volgende vacatures: circus ')
 
-
-
-
-
-solliciteren_baan = input('Je kan solliciteren voor de volgende banen. Kies uit de volgende vacaturen: circus ')
-
-if solliciteren_baan == 'circus':
-    rijbewijs =input ('ben je in bezit van een vrachtwagen rijbewijs? ')
+if solliciteren_baan.lower() == 'circus':
+    rijbewijs = input('Ben je in bezit van een vrachtwagen rijbewijs? ').lower()
     if rijbewijs == 'ja':
-        hoge_hoed = input('Ben je in bezit van een hoge hoed?  ')
+        hoge_hoed = input('Ben je in bezit van een hoge hoed?  ').lower()
         if hoge_hoed == 'ja':
             gewicht = int(input('Hoeveel weeg je? (kilo)'))
-            if gewicht >= MIN_GEWICHT and gewicht <= MAX_GEWICHT:
-                lengte= int(input('Hoe lang ben je? (cm)'))
-                if lengte >= MIN_LENGTE and lengte <= MAX_LENGTE:
-                    certificaat = input('Heb je een certificaat? ')
+            if MIN_GEWICHT <= gewicht <= MAX_GEWICHT:
+                lengte = int(input('Hoe lang ben je? (cm)'))
+                if MIN_LENGTE <= lengte <= MAX_LENGTE:
+                    certificaat = input('Heb je een certificaat? ').lower()
                     if certificaat == 'ja':
                         ervaring_dieren = int(input('Hoeveel jaar ervaring heb je in deze vakken, praktijkervaring met dieren-dressuur'))
-                        ervaring_jongeleren = int(input('Hoeveel jaar ervaring heb je in jongeleren'))
-                        ervaring_acrobatiek =  int(input('Hoeveel jaar ervaring heb je in acrobatiek? '))
-                        if ervaring_dieren >= 4 or ervaring_acrobatiek >= 3 or ervaring_jongeleren >= 5:
-                            print('Je kan doorgaan voor een gesprek. ')
+                        ervaring_jongleren = int(input('Hoeveel jaar ervaring heb je in jongleren'))
+                        ervaring_acrobatiek = int(input('Hoeveel jaar ervaring heb je in acrobatiek? '))
+                        if ervaring_dieren >= 4 or ervaring_acrobatiek >= 3 or ervaring_jongleren >= 5:
+                            print('Je kunt doorgaan voor een gesprek.')
+
+else:
+    print('Helaas je voldoet niet aan de eisen!')
