55d6b5c261b90d065bd20a290652592f6d846961
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2023-12-01 11:44:28 +0100

    klad verzamelingen

---
+++ module_1/klad/verzamelingen.py
@@ -0,0 +1,34 @@
+# mijn_lijst = []
+# persoon = {}
+
+# for _ in range(3):
+#     persoon['voornaam'] = input('wat is uw voornaam')
+#     persoon['achternaam'] = input('wat is uw achternaam')
+#     mijn_lijst.append(persoon)
+
+# print (mijn_lijst[1]['voornaam'])
+# mijn_lijst = []
+
+# autos_info = {}
+# for _ in range(2):
+#     autos_info['Merk'] = input('Wat is de merk van uw auto: ')
+#     autos_info['Prijs'] = int(input('Wat is de prijs van uw auto'))
+#     autos_info['Model'] = input('Welke model is uw auto')
+#     autos_info.append(mijn_lijst)
+
+# print(mijn_lijst['Prijs'])
+
+mijn_lijst = []
+
+for _ in range(2):
+    autos_info = {}  
+    autos_info['Merk'] = input('Wat is de merk van uw auto: ')
+    autos_info['Prijs'] = int(input('Wat is de prijs van uw auto: '))
+    autos_info['Model'] = input('Welke model is uw auto: ')
+    mijn_lijst.append(autos_info)  
+
+for autos_info in (mijn_lijst):
+    print(autos_info['Prijs'])
+
+
+
