7821e49a57e88fa978ad9afec583f132b38cb297
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-01-12 11:47:13 +0100

    Gelijkengetallen, tijd , lancering

---
+++ module_2/Deel 1/Lancering.py
@@ -0,0 +1,8 @@
+import time 
+for getal in range(30,0,-1):
+    print (getal)
+    time.sleep(1)
+    if getal == 1:
+        print ('Je raket is gelanceert')
+
+
---
+++ module_2/Deel 1/tijd.py
@@ -0,0 +1,10 @@
+# from time import sleep
+# for x in range(0,25):
+#     if x <= 12:
+#         print(f'Het is nu {x} am')
+#     else:
+#         print(f'Het is nu {x} pm')
+#     sleep(1)
+
+for i in range(0, 10, 2):
+    print(i)
