4bd8fee4ae73bf8a28d2996a60d82c95d1e0a7d8
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-01-16 09:13:21 +0100

    oefening6.py

---
+++ module_2/Deel 1/forever-young/oefening6.py
@@ -0,0 +1,10 @@
+    robotArm.moveLeft()
+    robotArm.drop()
+
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+robotArm.wait()
