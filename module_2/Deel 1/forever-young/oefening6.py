07964657e52a1e2ef5f1a18b669f2540b860d0e1
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-01-16 09:13:23 +0100

    Update oefening6.py

--- module_2/Deel 1/forever-young/oefening6.py
+++ module_2/Deel 1/forever-young/oefening6.py
@@ -1,3 +1,12 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+
+robotArm.moveRight()
+
+
+for _ in range (3):
+    robotArm.grab()
     robotArm.moveLeft()
     robotArm.drop()
 
