27ce04deeae6fb0e092966fbcbcb1cd2640e9d90
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-02-16 09:11:33 +0100

    Dagen.py opnieuw

--- module_2/Deel 2/dagen.py
+++ module_2/Deel 2/dagen.py
@@ -6,7 +6,13 @@ zin_twee = "Alle werkdagen van de week zijn: "
 
 zin_drie = "Alle weekend dagen zijn: "
 
-zin_vier = "De werkdagen in omgekeerde volgorde zijn:  "
+zin_vier = "Alle dagen in omgekeerde volgorde zijn:  "
+
+zin_vijf = "Alle werkdagen in omgekeerde volgorde zijn:  "
+
+zin_zes = "Alle weekenden in omgekeerde volgorde zijn:  "
+
+
 
 for dag in WEEK_DAGEN:
     zin_een += dag + ', '
@@ -21,8 +27,23 @@ for dag in WEEK_DAGEN[5:7]:
 for dag in WEEK_DAGEN[7::-1]: 
     zin_vier += dag +', '
 
-print (zin_een)
-print(zin_twee)
-print(zin_drie)
-print(zin_vier)
+for dag in WEEK_DAGEN[4::-1]: 
+    zin_vijf += dag +', '
 
+for dag in WEEK_DAGEN[6:4:-1]: 
+    zin_zes += dag +', '
+
+
+print('\n')
+print (zin_een)
+print('\n')
+print(zin_twee)
+print('\n')
+print(zin_drie)
+print('\n')
+print(zin_vier)
+print('\n')
+print(zin_vijf)
+print('\n')
+print(zin_zes)
+print('\n')
