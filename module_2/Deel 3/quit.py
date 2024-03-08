3b89303db25397a5b86358c8e2fe85a602cbdd5a
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-03-08 09:20:22 +0100

    quit.py

---
+++ module_2/Deel 3/quit.py
@@ -0,0 +1,8 @@
+aantal_keer_geantwoord = 0
+
+while True:
+    vraag = input('hoe voel je je? (QUIT) om te stoppen').lower()
+    aantal_keer_geantwoord += 1
+    if vraag == 'quit':
+        print(f'Je bent gestopt je hebt de vraag {aantal_keer_geantwoord -1} beantwoord')
+        break
