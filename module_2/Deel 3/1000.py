d8ac77228bbbdccb1ac2211db99f98e3e27aeaaa
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-03-08 11:52:56 +0100

    create 100.py

---
+++ module_2/Deel 3/1000.py
@@ -0,0 +1,19 @@
+total_sum = 0
+
+for current_number in range(50, 10000):
+    total_sum += current_number
+    text = ''
+
+    text += f"{current_number - 49}. "
+    current_sum = 0
+
+    for num in range(50, current_number + 1):
+        current_sum += num
+        text += f"{num}"
+        if num != current_number:
+            text += " + "
+
+    print(f"{text} = {current_sum}")
+
+    if total_sum > 1000:
+        break
