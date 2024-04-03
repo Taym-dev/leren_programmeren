099c6ee6c9239ae1e744aabc256268a37e4c39f6
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-04-03 12:01:28 +0200

    deel 5 opd 2

--- module_2/Deel 4/random_getal.py
+++ module_2/Deel 4/random_getal.py
@@ -17,27 +17,30 @@ while speelrondes < MAXIMAAL_AANTAL_SPEELRONDES:
         if gok_persoon_getal == getal_random:
             score_gebruiker += 1
             print('Je hebt het goed!')
-            print(f'Jouw score is {score_gebruiker}')
-            print("\n")
+            print(f'Jouw score is {score_gebruiker}\n')
             verder_raden = input('Wil je verder raden? (ja/nee): ').lower()
             if verder_raden != "ja":
                 print(f'Dit is je eindscore: {score_gebruiker}')
+                speelrondes = 20
                 break  
         elif aantal_speelrondes_in_een_ronde == 10:
             print("Dit was je laatste poging van deze ronde")
             print(f'Het juiste getal was: {getal_random}')
             print(f'Je hebt een score van {score_gebruiker}')
             break  
+        
+
         else:
             verschil = abs(gok_persoon_getal - getal_random)
+            if gok_persoon_getal < getal_random:
+                print('Hoger!')
+            else:
+                print('Lager!')
             if verschil < 20:
                 print('Je bent heel warm')
             elif verschil < 50:
                 print('Je bent warm')
-            elif gok_persoon_getal < getal_random:
-                print('Hoger!')
-            elif gok_persoon_getal > getal_random:
-                print('Lager!')
+
     
     speelrondes += 1
 
