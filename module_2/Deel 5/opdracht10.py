3b45ce75daeb568fd49a123578762ada4fea8511
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-04-04 12:06:09 +0200

    update opdracht10.py

--- module_2/Deel 5/opdracht10.py
+++ module_2/Deel 5/opdracht10.py
@@ -1,6 +1,14 @@
 from fruitmand import fruitmand
 
-fruitmand.sort(key=lambda fruit: fruit['weight'], reverse=True)
+# fruitmand.sort(key=lambda fruit: fruit['weight'], reverse=True)
+
+def mijnfunctie(fruit):
+    return fruit['weight']
+
+
+fruitmand.sort(key=mijnfunctie, reverse=True)
+
+
 
 for fruit in fruitmand:
     print(f"{fruit['name']}: {fruit['weight']}")
