49ff57700700552c1349420eeb52daca0361c847
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-04-04 10:05:05 +0200

    updated files

--- module_2/Deel 5/opdracht08.py
+++ module_2/Deel 5/opdracht08.py
@@ -1,11 +1,8 @@
 from fruitmand import fruitmand
 
-fruitmand = [fruit for fruit in fruitmand 
-                if fruit['name'] != 'druif']
+fruitmand = [fruit for fruit in fruitmand if fruit['name'] != 'druif']
 
 kleuren = set(fruit['color'] for fruit in fruitmand)
 
-# Druk alle verschillende kleuren af
-print("Alle verschillende kleuren in de fruitmand zijn:")
 for kleur in kleuren:
     print(kleur)
