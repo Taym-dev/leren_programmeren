98a173a793044f0860350149b240844c33b434bb
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-04-11 09:39:25 +0200

    Update Lootjes.py

--- module_2/eindopdracht/lootjes.py
+++ module_2/eindopdracht/lootjes.py
@@ -4,42 +4,43 @@ deelnemers = []
 
 
 def vraag_deelnemers():
-    while len(deelnemers) < 3:
-        naam = input("voer hier de namen in van de mensen ")
-        if naam not in deelnemers:
-            deelnemers.append(naam)
-        else:
-            print("Deze naam zit er al in doe een andere naam ")
+    naam = input("voer hier de namen in van de mensen ")
+    if naam not in deelnemers:
+        deelnemers.append(naam)
+    else:
+        print("Deze naam zit er al in doe een andere naam ")
        
 
 def trek_lootjes():
     lootjes = deelnemers.copy()
     random.shuffle(lootjes)
     while True:
-        lootjes_copy = lootjes.copy()
+        oke = True
         for i in range(len(deelnemers)):
             deelnemer = deelnemers[i]
-            lootje = lootjes_copy[i]
+            lootje = lootjes[i]
             if deelnemer == lootje:
                 random.shuffle(lootjes)
-                break
-        else:
+                oke = False
+        if oke == True:
+            return lootjes
+
+
+print("Voer de namen van deelnemers in minimaal 3:")
+
+
+while True:
+    vraag_deelnemers()
+    if  len(deelnemers) >= 3:
+        extra_naam = input("Wil je nog een naam toevoegen? ja/nee: ")
+        if extra_naam.lower() != 'ja':
             break
-    return lootjes
 
 
-print("Voer de namen van deelnemers in (minimaal 3):")
-vraag_deelnemers()
 
-if len(deelnemers) >= 3:
-    extra_naam = input("Wil je nog een naam toevoegen? (ja/nee): ")
-    if extra_naam.lower() == "ja":
-        vraag_deelnemers()
 
-    
-    lootjes = trek_lootjes()
-    print("Deelnemers en hun lootjes:")
-    for i in range(len(deelnemers)):
-        deelnemer = deelnemers[i]
-        lootje = lootjes[i]
-        print(f"{deelnemer}: {lootje}")
+lootjes = trek_lootjes()
+for i in range(len(deelnemers)):
+    deelnemer = deelnemers[i]
+    lootje = lootjes[i]
+    print(f"{deelnemer}: {lootje}")
