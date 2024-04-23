3ea1b6e11fdf383820412e1be985b9479b8703f4
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-04-23 11:19:34 +0200

    Create new Name.py

---
+++ module_3 /deel 1/name.py
@@ -0,0 +1,7 @@
+def vraag_naam_leeftijd():
+    naam_persoon = input('Vul uw naam in: ')
+    leeftijd_persoon = int(input("Vul uw leeftijd in: "))
+    return { "name": naam_persoon, "age": leeftijd_persoon}
+
+gebruiker_info = vraag_naam_leeftijd()
+print(gebruiker_info['name'],'is', gebruiker_info['age'],'jaar')
