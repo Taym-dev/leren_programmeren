81ffb7e26d6a86a4e5546f056fbd057aefb84846
Taym-dev <143711744+Taym-dev@users.noreply.github.com>
2024-05-16 10:17:54 +0200

    Create print_gegevens

---
+++ module_3 /deel 1/name/print_gegevens.py
@@ -0,0 +1,40 @@
+from gegevens_verzamelen import vraag_naam_leeftijd_woonplaats
+
+def verzamel_gegevens():
+    gegevens = []
+    while True:
+        invoer = input("Toets enter om door te gaan of stop om te printen: ")
+        if invoer.lower() == 'stop':
+            break
+        gegevens.append(vraag_naam_leeftijd_woonplaats())
+    return gegevens
+
+def print_resultaat(gebruikers_info):
+    print("\nResultaat:")
+    for info in gebruikers_info:
+        print(f"In {info['city']} woont de {info['age']} jarige {info['name']}")
+
+#info['city']       info['name']     info['age']
+        
+gebruikers_info = verzamel_gegevens()
+print_resultaat(gebruikers_info)
+
+
+def score(dice):
+    puntentelling = {1:1000, 6:600, 5:500, 4:400, 3:300, 2:200}
+    dobbelsteen_aantallen = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+
+    for worp in dice:
+        dobbelsteen_aantallen[worp] += 1
+
+    totaal_punten = 0
+
+    for waarde, aantal in dobbelsteen_aantallen.items():
+        if aantal >= 3:
+            totaal_punten += puntentelling[waarde] * (aantal // 3)
+        if waarde == 1:
+            totaal_punten += 100 * (aantal % 3)
+        elif waarde == 5:
+            totaal_punten += 50 * (aantal % 3)
+
+    return totaal_punten
